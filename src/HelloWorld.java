483952bf5e39b1a9e8c54ce7deb5b8120b12dcc2
Luis <luisme@luiss-mbp.lan>
2021-07-20 00:38:12 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+
+//       HW Java Types, Variables and Branching
+
+
+//        int DOG;
+        String CaT = "cat";
+
+//        System.out.println(DOG);
+        System.out.println(CaT);
+
+        myMethod();
+
+
+
+
+    }
+
+    static void myMethod(){
+        int Dog;
+        System.out.println(Dog);
+
+    }
+}
