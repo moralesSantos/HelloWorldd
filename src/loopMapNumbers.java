6ec27e53ddbff558f0be9006af9104888d0ba27e
luis <jmsantos879@gmail.com>
2021-07-21 16:29:14 -0400

    add smallest/largest algorithm

--- src/loopMapNumbers.java
+++ src/loopMapNumbers.java
@@ -25,10 +25,24 @@ public class loopMapNumbers {
             product*=num;
         }
 
+        smallest = list.get(0);
+        largest = list.get(0);
+        for(int i = 0; i <list.size(); i++){
+            if (list.get(i) < smallest){
+                smallest = list.get(i);
+            }
+        }
+        for(int i = 0; i <list.size(); i++){
+            if (list.get(i) > largest){
+                largest = list.get(i);
+            }
+        }
+
+
         System.out.println("The sum of all five numbers is: " + sum);
         System.out.println("The product of all five numbers is: " + product);
         System.out.println("The largest number is: " + Collections.max(list));
-        System.out.println("The smallest number is: " + Collections.min(list));
+        System.out.println("The smallest number is: " + smallest);
 
 
 
